00d3ffbc5e879c770c46444aed10bb9ad67d9047
kkmanuu <kipngenoemmanuel479@gmail.com>
2025-07-16 03:42:43 +0300

    Add HomeController for public pages and CoachingController for session handling

--- app/Http/Controllers/CoachingController.php
+++ app/Http/Controllers/CoachingController.php
@@ -6,5 +6,26 @@ use Illuminate\Http\Request;
 
 class CoachingController extends Controller
 {
-    //
+    /**
+     * Display a listing of the coaching sessions.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        // Logic to retrieve and display coaching sessions
+        return view('coaching');
+    }
+
+    /**
+     * Book a new coaching session.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function book(Request $request)
+    {
+        // Logic to handle booking a coaching session
+        return redirect()->route('coaching.index')->with('success', 'Coaching session booked successfully.');
+    }
 }
--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,5 +6,24 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('home');
+    }
+
+    public function about()
+    {
+        return view('about');
+    }
+
+    public function services()
+    {
+        return view('services');
+    }
+
+    public function contact()
+    {
+        return view('contact');
+}
+
 }
