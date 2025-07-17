d95e43b9101d2babe71edc5923dfb9c78eb95db0
kkmanuu <kipngenoemmanuel479@gmail.com>
2025-07-17 21:41:33 +0300

    Add a placeholder for admin

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,9 @@
+<!-- resources/views/admin/dashboard.blade.php -->
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <h1>Welcome Admin 👋</h1>
+        <p>This is your admin dashboard.</p>
+    </div>
+@endsection
