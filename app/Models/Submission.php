1f498e6610cea9e4537d1a25c98355fcc2690ad3
kkmanuu <kipngenoemmanuel479@gmail.com>
2025-07-20 20:10:48 +0300

    feat: create Submission model with project and feedback relationships

--- app/Models/Submission.php
+++ app/Models/Submission.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Submission extends Model
 {
     use HasFactory;
+     protected $fillable = ['project_id', 'score', 'status'];
+
+    public function project()
+    {
+        return $this->belongsTo(Project::class);
+    }
+
+    public function feedback()
+    {
+        return $this->hasOne(Feedback::class);
+    }
 }
