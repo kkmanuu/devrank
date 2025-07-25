04b5b3b74d99bab0e878440183c279e4c0d7c1b8
kkmanuu <kipngenoemmanuel479@gmail.com>
2025-07-25 15:26:23 +0300

    feat: add description and type fields to coaching_sessions table

---
+++ database/migrations/2025_07_23_124028_add_description_and_type_to_coaching_sessions_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+   public function up()
+    {
+        Schema::table('coaching_sessions', function (Blueprint $table) {
+            $table->text('description')->nullable()->after('topic');
+            $table->string('type')->nullable()->after('description');
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('coaching_sessions', function (Blueprint $table) {
+            $table->dropColumn(['description', 'type']);
+        });
+    }
+};
