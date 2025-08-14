59589b354125bb0788d88a6e2c6e05d1dc4bf862
kkmanuu <kipngenoemmanuel479@gmail.com>
2025-08-14 14:09:47 +0300

    Add 'amount' column to coaching_sessions table

---
+++ database/migrations/2025_08_11_202625_add_amount_to_coaching_sessions_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('coaching_sessions', function (Blueprint $table) {
+            $table->decimal('amount', 10, 2)->default(0.00)->after('status'); 
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('coaching_sessions', function (Blueprint $table) {
+            $table->dropColumn('amount');
+        });
+    }
+};
